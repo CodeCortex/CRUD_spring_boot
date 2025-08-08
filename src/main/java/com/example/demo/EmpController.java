c69f4481ff7002b05bbafcb00983a534664f3421
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2025-08-08 10:41:02 +0530

    created enitity and setup database

--- src/main/java/com/example/demo/EmpController.java
+++ src/main/java/com/example/demo/EmpController.java
@@ -6,18 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
+
+// @CrossOrigin("http://localhost:3000/")
 @RestController
 public class EmpController {
 
     // List<Employee> employees = new ArrayList<>();
-    EmployeeService employeeService = new EmployeeServiceImp1();
+    @Autowired
+    private EmployeeService employeeService;
 
     // dependency injection
     // @Autowired
@@ -27,6 +32,12 @@ public class EmpController {
     public List<Employee> getAllEmployees() {
         return employeeService.readEmployees();
     }
+
+    @GetMapping("employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.readEmployee(id);
+    }
+    
 
     @PostMapping("employees")
     public String createEmployee(@RequestBody Employee emp) {
@@ -43,5 +54,14 @@ public class EmpController {
         return "Not Found";
 
     }
+
+    @PutMapping("employees/{id}")
+    public String putMethodName(@PathVariable Long id, @RequestBody Employee employee) {
+        //TODO: process PUT request
+        return employeeService.updateEmployee(id, employee);
+        
+    }
+
+
 
 }
--- src/main/java/com/example/demo/EmployeeServiceImp1.java
+++ src/main/java/com/example/demo/EmployeeServiceImp1.java
@@ -3,28 +3,75 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service  // Needed for @Autowired to work
+@Service // Needed for @Autowired to work
 public class EmployeeServiceImp1 implements EmployeeService {
 
-    List<Employee> employees = new ArrayList<>();
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    // List<Employee> employees = new ArrayList<>();
 
     @Override
     public String createEmployee(Employee employee) {
-        employees.add(employee);
+        // employees.add(employee);
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        BeanUtils.copyProperties(employee, employeeEntity);
+        employeeRepository.save(employeeEntity);
         return "Saved Successfully";
     }
 
     @Override
     public List<Employee> readEmployees() {
+        List<EmployeeEntity> employeeList = employeeRepository.findAll();
+        List<Employee> employees = new ArrayList<>();
+
+        for (EmployeeEntity employeeEntity : employeeList) {
+            Employee emp = new Employee();
+            emp.setId(employeeEntity.getId());
+            emp.setName(employeeEntity.getName());
+            emp.setEmail(employeeEntity.getEmail());
+            emp.setPhone(employeeEntity.getPhone());
+            employees.add(emp);
+        }
         return employees;
     }
 
     @Override
     public boolean deleteEmployee(Long id) {
-        return employees.removeIf(emp -> emp.getId().equals(id));
-        // employees.remove(id);
-        // return true;
+        // EmployeeEntity emp= employeeRepository.findById(id).get();
+        // employeeRepository.delete(emp);
+
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
+
+    @Override
+    public String updateEmployee(Long id, Employee employee) {
+        // TODO Auto-generated method stub
+        EmployeeEntity existingEmployee = employeeRepository.findById(id).get();
+        
+        existingEmployee.setEmail(employee.getEmail());
+        existingEmployee.setName(employee.getName());
+        existingEmployee.setPhone(employee.getPhone());
+
+        employeeRepository.save(existingEmployee);
+
+        return "Update Successfully";
+    }
+
+    @Override
+    public Employee readEmployee(Long id) {
+        // TODO Auto-generated method stub
+        EmployeeEntity employeeEntity= employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
+    }
+
 }
